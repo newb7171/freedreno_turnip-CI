47cdc77a37257cdc7179e4216fdb170ddb3a8b33
newb7171 <rotheory13@gmail.com>
2026-06-17 20:08:51 +0800

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -23,11 +23,7 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 		
 	echo "Pushing TU_VERSION..."
 	echo "#define TUGEN8_DRV_VERSION \"v$BUILD_VERSION\"" > ./src/freedreno/vulkan/tu_version.h
-	#Workaround for using Clang as c compiler instead of GCC
-	mkdir -p "$workdir/bin"
-	ln -sf "$ndk/clang" "$workdir/bin/cc"
-	ln -sf "$ndk/clang++" "$workdir/bin/c++"
-	export PATH="$workdir/bin:$ndk:$PATH"
+	
 	export CC=clang
 	export CXX=clang++
 	export AR=llvm-ar
@@ -41,8 +37,8 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 		cat <<EOF >"android-aarch64.txt"
 [binaries]
 ar = '$ndk/llvm-ar'
-c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang']
-cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments']
+c = '$ndk/aarch64-linux-android34-clang'
+cpp = [$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments']
 c_ld = '$ndk/ld.lld'
 cpp_ld = '$ndk/ld.lld'
 strip = '$ndk/llvm-strip'
